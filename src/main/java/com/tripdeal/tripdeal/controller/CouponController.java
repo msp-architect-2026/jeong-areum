f582bacb2c6cdaebc1c4c6a278f7168c8c2e2d95
armddi <158549123+armddi@users.noreply.github.com>
2026-03-09 21:06:16 +0900

    Update CouponController.java

--- src/main/java/com/tripdeal/tripdeal/controller/CouponController.java
+++ src/main/java/com/tripdeal/tripdeal/controller/CouponController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/coupons")
-@CrossOrigin(origins = "*")
+
 public class CouponController {
 
     private final CouponRepository couponRepository;
